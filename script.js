9eb25ab37c7c374188edf4bcecb4e36f1f354499
James Kopcienski <jkopcienski@gmail.com>
2013-06-28 12:31:33 -0400

    final commit

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ jQuery(function($) {
     $('.selected').removeClass('selected');
     $(this).addClass('selected');                  //highlight selected loan
 
-    var loanNum = $(this).data('loannum');
-    var lenderId = $(this).data('lenid');
+    var lenderId = $(this).data('lenid');          //data from selected loan
     var lenderName = $(this).data('lenname');
     var lenderLocation = $(this).data('lenloc');
     var lenderImage = $(this).data('lenimage');
@@ -27,7 +26,6 @@ jQuery(function($) {
     var borrowerLocation = $(this).data('borloc');
     var borrowerCountry = $(this).data('borcntry');
     var borrowerImage = $(this).data('borimage');
-    console.log(loanNum, lenderLocation, borrowerLocation);
 
     geocoder.geocode( { 'address': lenderLocation}, function(results, status) {  //get lender geo coordinates; returns as an object
       if (status == google.maps.GeocoderStatus.OK) {
@@ -42,9 +40,9 @@ jQuery(function($) {
         var borLatLng = new google.maps.LatLng(borrowerLocation.split(' ')[0], borrowerLocation.split(' ')[1]);          //split borrower geo coordinates string into latitude and longitude
         var loanLineCoordinates = [lenderLatLng, borLatLng];                      //set coordinates for polyline
 
-        var loanLine = new google.maps.Polyline({            //draw line between lender and borrower
+        var loanLine = new google.maps.Polyline({            //set properties for line between lender and borrower
           path: loanLineCoordinates,
-          strokeColor: '#FF0000',
+          strokeColor: '#4b9123',
           strokeOpacity: 1.0,
           strokeWeight: 2
         });
@@ -53,33 +51,38 @@ jQuery(function($) {
         for (var i = 0; i < loanLineCoordinates.length; i++) {
           bounds.extend(loanLineCoordinates[i]);
         }
-        map.fitBounds(bounds);
+        
+        map.fitBounds(bounds);                               //draws the lines and sets the boundary and zoom level
         loanLine.setMap(map);
 
         var lenderMarker = new google.maps.Marker({          //add markers at the ends of the polyline
           map: map,
           position: lenderLatLng,
+          icon: 'images/green_MarkerL.png',
           title: 'Lender'
         });
         var borrowerMarker = new google.maps.Marker({
           map: map,
           position: borLatLng,
+          icon: 'images/green_MarkerB.png',
           title: 'Borrower'
         });
-        mapArray.push(loanLine, lenderMarker, borrowerMarker);
+        mapArray.push(loanLine, lenderMarker, borrowerMarker); //save map data for later clearing
         
         var lenderString = '<div id="lenderWindow">' +       //pop-up windows
-                          '<p class="winTitle">Lender</p>' +
-                          '<p class="winName">' + lenderName + '</p>' +
-                          '<p class="winLoc">' + lenderLocation + '</p>' +
+                          '<p class="wintitle">Lender</p>' +
+                          '<p class="winname">' + lenderName + '</p>' +
+                          '<p class="winloc">' + lenderLocation + '</p>' +
                           '<a href="http://www.kiva.org/lender/' + lenderId + '" target="_blank"><img src="http://www.kiva.org/img/w200h200/' + lenderImage + '.jpg"></a>' +
+                          '<p class="wincaption">Click photo for more info on kiva.org</p>' +
                           '</div>';
   
         var borrowerString = '<div id="borrowerWindow">' +
-                          '<p class="winTitle">Borrower</p>' +
-                          '<p class="winName">' + borrowerName + '</p>' +
-                          '<p class="winLoc">' + borrowerCountry + '</p>' +
+                          '<p class="wintitle">Borrower</p>' +
+                          '<p class="winname">' + borrowerName + '</p>' +
+                          '<p class="winloc">' + borrowerCountry + '</p>' +
                           '<a href="http://www.kiva.org/lend/' + borrowerId + '" target="_blank"><img src="http://www.kiva.org/img/w200h200/' + borrowerImage + '.jpg"></a>' +
+                          '<p class="wincaption">Click photo for more info on kiva.org</p>' +
                           '</div>';
   
         var lenderWindow = new google.maps.InfoWindow({
@@ -111,37 +114,47 @@ jQuery(function($) {
       dataType: 'jsonp',
       jsonp: 'jsonp',
       cache: false,
-      beforeSend: function() {    //empty previous list, display loading text, hide refresh button
+      timeout: 10000,
+      beforeSend: function() {                    //empty previous list, display loading text, hide refresh button
         $('#recentlist').empty();
         $('h2').text('Loading loans.....');
         $('#refresh').hide();
       },
-      complete: function() {     //display list header
+      complete: function() {                      //display list header
         $('h2').text('Here are ten of the most recent loans made on Kiva. Choose one to display it on the map.');
       },
       success: function(recentLoans) {
         console.log(recentLoans);
         var listNum = 0;
-        for (var i = 0; i < 100; i+=3) {   //array contains 100 objects, increment by 3 to avoid repeat lenders
+        var prevLender = "";
+        for (var i = 0; i < 100; i++) {   //array contains 100 objects
           var lenWhere = recentLoans.lending_actions[i].lender.whereabouts;
-          var loanNum = 'data-loannum="' + i + '"';
           var lenderId = ' data-lenid="' + recentLoans.lending_actions[i].lender.lender_id + '"';
-          var lenderName = ' data-lenname="' + recentLoans.lending_actions[i].lender.name + '"';
-          var lenderLocation = ' data-lenloc="' + lenWhere + '"';
-          var lenderImage = ' data-lenimage="' + recentLoans.lending_actions[i].lender.image.id + '"';
-          var borrowerId = ' data-borid="' + recentLoans.lending_actions[i].loan.id + '"';
-          var borrowerName = ' data-borname="' + recentLoans.lending_actions[i].loan.name + '"';
-          var borrowerLocation = ' data-borloc="' + recentLoans.lending_actions[i].loan.location.geo.pairs + '"';
-          var borrowerCountry = ' data-borcntry="' + recentLoans.lending_actions[i].loan.location.country + '"';
-          var borrowerImage = ' data-borimage="' + recentLoans.lending_actions[i].loan.image.id + '"';
-          var loanDesc = recentLoans.lending_actions[i].loan.activity + ": " + recentLoans.lending_actions[i].loan.use;
-          if ((!lenWhere  == "") && (listNum < 10)) {    //skip lenders with no location, limit list to 10 items
-            $('#recentlist').append('<a class="loans"' + loanNum + borrowerId + borrowerName + borrowerLocation + borrowerCountry + borrowerImage + lenderId + lenderName + lenderLocation + lenderImage + '><li>' + loanDesc + '</li></a>'); //use data- to send data to displayMap function
+          if ((lenWhere !== "") && (lenderId !== prevLender) && (listNum < 10)) {    //skip lenders with no location and repeat lenders; limit list to 10 items
+            
+            var lenderName = ' data-lenname="' + recentLoans.lending_actions[i].lender.name + '"';     //data to load into each link
+            var lenderLocation = ' data-lenloc="' + lenWhere + '"';
+            var lenderImage = ' data-lenimage="' + recentLoans.lending_actions[i].lender.image.id + '"';
+            var borrowerId = ' data-borid="' + recentLoans.lending_actions[i].loan.id + '"';
+            var borrowerName = ' data-borname="' + recentLoans.lending_actions[i].loan.name + '"';
+            var borrowerLocation = ' data-borloc="' + recentLoans.lending_actions[i].loan.location.geo.pairs + '"';
+            var borrowerCountry = ' data-borcntry="' + recentLoans.lending_actions[i].loan.location.country + '"';
+            var borrowerImage = ' data-borimage="' + recentLoans.lending_actions[i].loan.image.id + '"';
+            var loanDesc = recentLoans.lending_actions[i].loan.activity + ": " + recentLoans.lending_actions[i].loan.use;
+            
+            $('#recentlist').append('<a class="loans"' + borrowerId + borrowerName + borrowerLocation + borrowerCountry + borrowerImage + lenderId + lenderName + lenderLocation + lenderImage + '><li>' + loanDesc + '</li></a>'); //use data- to send data to displayMap function
+
             listNum++;
+            prevLender = lenderId;
           }
         }
-        $('#refresh').show();
+        $('#refresh').show();                                 //display refresh list button
+        
         $('#recentlist').on('click', '.loans', displayMap);   //display loan on the map when it is selected
+      },
+      error: function(recentLoans) {
+        $('h2').text('Hmmmm....there was some difficulty in getting the data from kiva.org. Click the Refresh List button to try again.');
+        $('#refresh').show();
       }
     });
   }
